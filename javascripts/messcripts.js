c245d723e488f27d29e708284546205cbe1e9300
Noordo <contact@noordover.fr>
2016-02-12 16:24:59 +0100

    Rectification d'un lien et suppression d'un cas inutile au niveau du javascript.

--- javascripts/messcripts.js
+++ javascripts/messcripts.js
@@ -7,17 +7,8 @@ $(document).ready(function(){
     
     switch(window.location.hostname) {
         case "isn.noordover.fr":
-            $("body > header > p").prepend("Dernière version supervisée (VO) du ");
             $(".VO").css("display","inline-block");
             $(".VD").css("display","none");
-            break;  
-        case "isnnoordo.github.io":
-            document.title = 'DEV-'+document.title;
-            $("body > header > h1").prepend("DEV-");
-            $("body > header > h1").css("background-color","red");
-            $("body > header > p").prepend("Version de développement (VDev) du ");
-            $(".VO").css("display","none");
-            $(".VD").css("display","inline-block");
             break;  
         default:
             document.title = 'LOC-'+document.title;
